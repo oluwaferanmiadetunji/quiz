0b10b94bbbd244c91632a9a078b8f53b30dbbc2f
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-23 09:11:22 +0100

    .

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,10 +4,12 @@ const loginUser = require('../controllers/loginUser');
 const loginAdmin = require('../controllers/loginAdmin');
 const registerUser = require('../controllers/registerUser');
 const registerAdmin = require('../controllers/registerAdmin');
+const resetPassword = require('../controllers/resetPassword');
 
 router.post('/user/login', loginUser);
 router.post('/user/register', registerUser);
 router.post('/admin/login', loginAdmin);
 router.post('/admin/register', registerAdmin);
+router.post('/reset', resetPassword);
 
 module.exports = router;
