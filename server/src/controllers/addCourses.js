162642847b80dfbf6b5d2bc9d0d55ee63bfec6c4
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-23 12:17:16 +0100

    .

--- server/src/controllers/addCourses.js
+++ server/src/controllers/addCourses.js
@@ -1,5 +1,26 @@
-const { db, firebase } = require('../config/firebase');
-const validateAdminData = require('../helpers/validateAdminData');
-const { USER_EXISTS, USER_EXISTS_RESPONSE, SUCCESS, FAILURE, USER_CREATED } = require('../config/constants');
+const { db } = require('../config/firebase');
+const { SUCCESS, FAILURE, SUCCESS_MESSAGE } = require('../config/constants');
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+	const course = req.body.course.trim();
+	if (course === '')
+		return res.status(417).json({
+			status: FAILURE,
+			message: 'Course can not be empty',
+			data: '',
+		});
+	try {
+		await db.collection('courses').add({ course, createdAt: new Date().toISOString() });
+		return res.status(201).json({
+			status: SUCCESS,
+			message: SUCCESS_MESSAGE,
+			data: '',
+		});
+	} catch (err) {
+		return res.status(500).json({
+			status: FAILURE,
+			message: GENERAL_ERROR,
+			data: err,
+		});
+	}
+};
