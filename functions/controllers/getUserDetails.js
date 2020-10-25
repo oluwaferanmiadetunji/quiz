27fc7d0c923af0754cffac3d515461f387f326bd
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-26 00:30:54 +0100

    .

--- functions/controllers/getUserDetails.js
+++ functions/controllers/getUserDetails.js
@@ -1,4 +1,4 @@
-const {db} = require('../config/firebase');
+const { db } = require('../config/firebase');
 
 module.exports = async (req, res) => {
 	const uid = req.user.uid;
@@ -6,5 +6,17 @@ module.exports = async (req, res) => {
 	await db.ref(`user/${uid}`).once('value', (snapshot) => {
 		userData = snapshot.val();
 	});
-	res.status(200).json({status: 'ok', message: 'Successful', data: {uid, ...userData}});
+	const history = userData.history;
+	const key = Object.keys(history);
+	const value = Object.values(history);
+
+	let array = [];
+	for (let i = 0; i < key.length; i++) {
+		const data = {
+			key: key[i],
+			data: value[i],
+		};
+		array.push(data);
+	}
+	res.status(200).json({ status: 'ok', message: 'Successful', data: { uid, ...userData, history: array } });
 };
