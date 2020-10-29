d46e5733625a022c1269153995540f8b9f836d68
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2020-10-29 08:43:22 +0100

    Modify login response

--- functions/controllers/loginUser.js
+++ functions/controllers/loginUser.js
@@ -1,19 +1,19 @@
-const {admin, db, firebase} = require('../config/firebase');
+const { admin, db, firebase } = require('../config/firebase');
 const validateLoginData = require('../helpers/validateLoginData');
-const {NO_USER, WRONG_PASSWORD, SUCCESS, FAILURE, NO_USER_RESPONSE, USER_LOGGED, GENERAL_ERROR} = require('../constants');
+const { NO_USER, WRONG_PASSWORD, SUCCESS, FAILURE, NO_USER_RESPONSE, USER_LOGGED, GENERAL_ERROR } = require('../constants');
 
 module.exports = async (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password;
 
 	// validate data
-	const validateParams = validateLoginData({email, password});
+	const validateParams = validateLoginData({ email, password });
 
 	if (validateParams.error) {
-		return res.status(417).json({status: FAILURE, message: validateParams.message, data: ''});
+		return res.status(417).json({ status: FAILURE, message: validateParams.message, data: '' });
 	}
 
-	let token, uid;
+	let token, uid, userData;
 	firebase
 		.auth()
 		.signInWithEmailAndPassword(email, password)
@@ -28,13 +28,27 @@ module.exports = async (req, res) => {
 			});
 		})
 		.then(() => {
-			return !!userData
-				? res.status(200).json({
-						status: SUCCESS,
-						message: USER_LOGGED,
-						data: token,
-				  })
-				: res.status(417).json({status: FAILURE, message: NO_USER_RESPONSE, data: ''});
+			if (!!userData) {
+				const history = userData.history;
+				const key = Object.keys(history);
+				const value = Object.values(history);
+
+				let array = [];
+				for (let i = 0; i < key.length; i++) {
+					const data = {
+						key: key[i],
+						data: value[i],
+					};
+					array.push(data);
+				}
+				return res.status(200).json({
+					status: SUCCESS,
+					message: USER_LOGGED,
+					data: { token, user: { ...userData, history: array } },
+				});
+			} else {
+				return res.status(417).json({ status: FAILURE, message: NO_USER_RESPONSE, data: '' });
+			}
 		})
 		.then(() => {
 			db.ref(`user/${uid}`).update({
@@ -43,11 +57,11 @@ module.exports = async (req, res) => {
 		})
 		.catch((err) => {
 			if (err.code === NO_USER) {
-				return res.status(417).json({status: FAILURE, message: NO_USER_RESPONSE, data: ''});
+				return res.status(417).json({ status: FAILURE, message: NO_USER_RESPONSE, data: '' });
 			} else if (err.code === WRONG_PASSWORD) {
-				return res.status(417).json({status: FAILURE, message: NO_USER_RESPONSE, data: ''});
+				return res.status(417).json({ status: FAILURE, message: NO_USER_RESPONSE, data: '' });
 			} else {
-				return res.status(417).json({status: FAILURE, message: GENERAL_ERROR, data: ''});
+				return res.status(417).json({ status: FAILURE, message: GENERAL_ERROR, data: '' });
 			}
 		});
 };
