d107651a1d88855f78fd4b430b0ee16f498dbee9
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2020-12-09 18:13:47 +0100

    .

--- functions/middlewares/Auth.js
+++ functions/middlewares/Auth.js
@@ -12,18 +12,20 @@ module.exports = (req, res, next) => {
 		.verifyIdToken(idToken)
 		.then((decodedToken) => {
 			req.user = decodedToken;
-			db.collection('users')
-				.doc(req.user.uid)
+			return db
+				.collection('users')
+				.where('userId', '==', req.user.uid)
+				.limit(1)
 				.get()
-				.then((doc) => {
-					req.user.email = doc.data().email;
-					req.user.name = doc.data().name;
-					req.user.activated = doc.data().activated;
-					req.user.count = doc.data().count;
+				.then((data) => {
+					req.user.email = data.docs[0].data().email;
+					req.user.name = data.docs[0].data().name;
+					req.user.activated = data.docs[0].data().activated;
+					req.user.count = data.docs[0].data().count;
+					return next();
+				})
+				.catch(() => {
+					return res.status(403).json({ status: 'error', message: 'Error while verifying token', data: '' });
 				});
-			return next();
-		})
-		.catch(() => {
-			return res.status(403).json({ status: 'error', message: 'Error while verifying token', data: '' });
 		});
 };
